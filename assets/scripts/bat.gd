53bbee17f391eb050c9a2159f8528e5f3bfe0b35
Menace791 <manas.s.sara@gmail.com>
2023-11-10 17:10:36 -0800

    bat swing s better

--- assets/scripts/bat.gd
+++ assets/scripts/bat.gd
@@ -1,14 +1,19 @@
 extends MeshInstance3D
 
-var swingDistance = 90
-var swingTime = 1.0
+var swingTime = 0.2
+
+var swingDegrees = 120.0
+var ogRotation = get_rotation_degrees()
+var endRotation = Vector3(ogRotation.x, ogRotation.y+swingDegrees, ogRotation.z)
+
 var tween: Tween
 
 func _input(event):
-	if event.is_action_pressed("swing"):
+	if (event.is_action_pressed("swing") && (get_rotation_degrees() == ogRotation)):
 		swing()
 
 func swing():
-#	tween = create_tween()
-#	tween.tween_property(self, "rotation_degrees", swingDistance, swingTime)
+	tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", endRotation, swingTime).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "rotation_degrees", ogRotation, swingTime).set_trans(Tween.TRANS_SINE)
 	print("YOOOOOO")
