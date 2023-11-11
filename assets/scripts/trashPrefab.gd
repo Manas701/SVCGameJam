54c050644af17e86b020631d784dd55c6e5203f6
Menace791 <manas.s.sara@gmail.com>
2023-11-11 15:28:00 -0800

    .

--- assets/scripts/trashPrefab.gd
+++ assets/scripts/trashPrefab.gd
@@ -5,8 +5,7 @@ var trash_type: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.connect("animation_finished", destroy_trash)
-	
+	anim.connect("animation_finished", on_trash_landed)
 	randomize()
 	var trashOptions = get_tree().get_nodes_in_group("trashObjects")
 	var trash = trashOptions[randi() % trashOptions.size()]
@@ -20,5 +19,5 @@ func hit():
 #	anim.play("hit_center")
 	anim.play(get_parent().get_node("PlayerInput").whichBin)
 
-func destroy_trash(_event):
+func on_trash_landed(_event):
 	queue_free()
