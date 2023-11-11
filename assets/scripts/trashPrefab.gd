e9459a8f7a181b8dd89daac68bbb3ac98f7a4469
Menace791 <manas.s.sara@gmail.com>
2023-11-11 15:45:46 -0800

    🐢🐢🐢🐢🐢🐢🐢🐢

--- assets/scripts/trashPrefab.gd
+++ assets/scripts/trashPrefab.gd
@@ -2,14 +2,15 @@ extends Node
 
 @onready var anim = $AnimationPlayer
 var trash_type: Node
+var chosenTrash
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim.connect("animation_finished", on_trash_landed)
 	randomize()
 	var trashOptions = get_tree().get_nodes_in_group("trashObjects")
-	var trash = trashOptions[randi() % trashOptions.size()]
-	trash.set_visible(true)
+	chosenTrash = trashOptions[randi() % trashOptions.size()]
+	chosenTrash.set_visible(true)
 
 func _input(event):
 	if event.is_action_pressed("swing") && !anim.is_playing():
@@ -17,7 +18,11 @@ func _input(event):
 
 func hit():
 #	anim.play("hit_center")
-	anim.play(get_parent().get_node("PlayerInput").whichBin)
+	anim.play(get_parent().get_node("PlayerInput").whichAnim)
 
 func on_trash_landed(_event):
+	if (chosenTrash.trash_type == get_parent().get_node(get_parent().get_node("PlayerInput").whichBin).trash_type):
+		print("Right Bin! :)")
+	else:
+		print("Wrong Bin! :(")
 	queue_free()
--- scenes/ChangingBins.gd
+++ scenes/ChangingBins.gd
@@ -1,17 +1,22 @@
 extends Node
 
 var binAnims = ["hit_left", "hit_center", "hit_right"]
+var binTypes = ["Bin_Trash", "Bin_Recycle", "Bin_Compost"]
 var selectedAnim = 1
-@export var whichBin: String
+@export var whichAnim: String
+@export var whichBin: String 
 
 func _ready():
-	whichBin = binAnims[selectedAnim]
+	whichAnim = binAnims[selectedAnim]
+	whichBin = binTypes[selectedAnim]
 
 func _input(event):
 	if (event.is_action_pressed("select_left") && selectedAnim > 0):
 		selectedAnim -= 1
-		whichBin = binAnims[selectedAnim]
+		whichAnim = binAnims[selectedAnim]
+		whichBin = binTypes[selectedAnim]
 		
 	if (event.is_action_pressed("select_right") && selectedAnim < 2):
 		selectedAnim += 1
-		whichBin = binAnims[selectedAnim]
+		whichAnim = binAnims[selectedAnim]
+		whichBin = binTypes[selectedAnim]
